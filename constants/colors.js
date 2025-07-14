96dcbfc8d30239b5dccb6fc0eea5b75a60fc8da6
Fazal <meetfazalkarim@gmail.com>
2025-07-14 16:26:29 +0500

    Added color.js file

---
+++ constants/colors.js
@@ -0,0 +1,12 @@
+export const COLORS = {
+  primary: '#1A237E',
+  accent: '#FFD700',
+  background: '#F8FAFF',
+  card: '#fff',
+  border: '#F0F4FF',
+  shadow: '#1A237E',
+  tipBg: '#FFF8E1',
+  tipBorder: '#FFE082',
+  promoBg: '#1A237E',
+  promoAccent: '#FFD700',
+}; 
