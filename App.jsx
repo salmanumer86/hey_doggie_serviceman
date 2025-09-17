61246e4cbd4d7f0e509ae69e4fd083756db1de52
Salman Umer <salmanumer.dev@gmail.com>
2025-09-17 15:24:30 +0500

    Remove React import from App.jsx

--- App.jsx
+++ App.jsx
@@ -1,7 +1,13 @@
+import { Provider } from 'react-redux';
+import store from './redux/store';
 import AppNavigator from './navigation/navigation';
 
 const App = () => {
-  return <AppNavigator />;
+  return (
+    <Provider store={store}>
+      <AppNavigator />
+    </Provider>
+  );
 };
 
 export default App;
