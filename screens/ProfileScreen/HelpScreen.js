adde0c64b6c974ffabe94cbe853f214a3b710005
Salman Umer <salmanumer.dev@gmail.com>
2025-09-17 15:25:43 +0500

    Remove React import from Profile HelpScreen

---
+++ screens/ProfileScreen/HelpScreen.js
@@ -0,0 +1,9 @@
+import { View, Text } from 'react-native';
+
+const HelpScreen = () => (
+  <View style={{ flex: 1, justifyContent: 'center', alignItems: 'center' }}>
+    <Text>Help Screen</Text>
+  </View>
+);
+
+export default HelpScreen; 
