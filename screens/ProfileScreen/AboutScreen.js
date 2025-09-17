a66d08e6683d95a36bfa180b909c16994799a574
Salman Umer <salmanumer.dev@gmail.com>
2025-09-17 15:25:35 +0500

    Remove React import from Profile AboutScreen

---
+++ screens/ProfileScreen/AboutScreen.js
@@ -0,0 +1,9 @@
+import { View, Text } from 'react-native';
+
+const AboutScreen = () => (
+  <View style={{ flex: 1, justifyContent: 'center', alignItems: 'center' }}>
+    <Text>About Screen</Text>
+  </View>
+);
+
+export default AboutScreen; 
