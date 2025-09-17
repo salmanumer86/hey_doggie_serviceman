bd71ec44612e72e9959ec33f7077f86e430b91eb
Salman Umer <salmanumer.dev@gmail.com>
2025-09-17 15:25:50 +0500

    Remove React import from Profile PrivacyScreen

---
+++ screens/ProfileScreen/PrivacyScreen.js
@@ -0,0 +1,9 @@
+import { View, Text } from 'react-native';
+
+const PrivacyScreen = () => (
+  <View style={{ flex: 1, justifyContent: 'center', alignItems: 'center' }}>
+    <Text>Privacy Screen</Text>
+  </View>
+);
+
+export default PrivacyScreen; 
