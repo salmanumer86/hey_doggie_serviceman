8eba3f7a172896dafbc1611865d384f8b301ab45
Salman Umer <salmanumer.dev@gmail.com>
2025-09-18 17:54:45 +0500

    updated index.js file

---
+++ screens/index.js
@@ -0,0 +1,10 @@
+export { default as AuthScreen } from './AuthScreen/AuthScreen';
+export { default as HomeScreen } from './HomeScreen/HomeScreen';
+export { default as RequestDetailsScreen } from './RequestDetailsScreen/RequestDetailsScreen';
+export { default as EarningsScreen } from './EarningsScreen/EarningsScreen';
+export { default as ProfileScreen } from './ProfileScreen/ProfileScreen';
+export { default as NotificationsScreen } from './NotificationsScreen/NotificationsScreen';
+export { default as SettingsScreen } from './SettingsScreen/SettingsScreen';
+export {default as RecentActivityScreen} from './RecentActivityScreen/RecentActivityScreen'
+export {default as BookingOverviewScreen} from './BookingOverviewScreen/BookingOverviewScreen'
+export {default as SupportScreen} from './SupportScreen/SupportScreen'
